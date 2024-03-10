64adbc7670903671c547f7fecdb8297a5499e946
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-11 09:19:51 +1300

    first commit

---
+++ connect.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+db = sqlite3.connect("splatoon3.db")
+cursor = db.cursor()
+sql = "SELECT * FROM MainWeapon;"
+cursor.execute(sql)
+results = cursor.fetchall()
+
+for name in results:
+    if name[1] != None:
+        print(name[1])
+
+db.close()
