61f7b78470d71c293f8e9a117cb3b749c8e303a0
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-05 10:28:27 +1300

    finished data

--- connect.py
+++ connect.py
@@ -1,13 +1,21 @@
 import sqlite3
 
-db = sqlite3.connect("splatoon3.db")
-cursor = db.cursor()
-sql = "SELECT * FROM MainWeapon;"
-cursor.execute(sql)
-results = cursor.fetchall()
 
-for name in results:
-    if name[1] is not None:
-        print(name[1])
+def connect(query):
+    db = sqlite3.connect("splatton3.db")
+    cursor = db.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    print_result(results)
+    return results
+    db.close()
 
-db.close()
+
+def print_result(results):
+    for i in results:
+        if results.type == list:
+            if i[1] is not None:
+                print(i[1])
+
+
+connect("SELECT * FROM Weapons")
