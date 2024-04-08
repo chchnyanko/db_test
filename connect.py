49d625493745392a33fa567fe29f68e35975d6d9
chchnyanko <matthiroki@gmail.com>
2024-04-08 14:36:33 +1200

    fixed the data and added some code

--- connect.py
+++ connect.py
@@ -1,21 +1,46 @@
 import sqlite3
 
 
+is_id = ""
+
+
+def check_id(id: str):
+    print(id)
+    target_id = id.replace("ID", "Name")
+    id_table = id.replace("ID", "")
+    print(f"SELECT {target_id} FROM {id_table};")
+    connect(f"SELECT {target_id} FROM {id_table};")
+
+
 def connect(query):
-    db = sqlite3.connect("splatton3.db")
+    db = sqlite3.connect("splatoon3.db")
     cursor = db.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
     print_result(results)
-    return results
     db.close()
+    return results
 
 
 def print_result(results):
+    print(is_id)
     for i in results:
-        if results.type == list:
-            if i[1] is not None:
-                print(i[1])
+        if i is not None:
+            if type(i) == tuple:
+                i = i[0]
+            print(i)
 
 
-connect("SELECT * FROM Weapons")
+target = input("What are you looking for ")
+table = input("Where is it from ")
+limit = input("Do you want any filters leave blank if not ")
+filter = ""
+if target.lower().find("id") != -1:
+    is_id = target
+if limit:
+    filter = (f" WHERE {limit}")
+if is_id:
+    check_id(is_id)
+else:
+    connect(f"SELECT {target} FROM {table}{filter}")
+
---
+++ fix main weapon names.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+
+
+db = sqlite3.connect("splatoon3.db")
+cursor = db.cursor()
+cursor.execute("SELECT MainWeaponName FROM MainWeapon;")
+results = cursor.fetchall()
+for i in range(len(results)):
+    print(results[i][0])
+    if results[i][0].find(" ") == 0:
+        query = "UPDATE MainWeapon SET MainWeaponName = ? WHERE MainWeaponID = ?;"
+        cursor.execute(query, [str(results[i][0].replace(" ", "")), int(i + 1)])
+db.commit()
+
+db.close()
