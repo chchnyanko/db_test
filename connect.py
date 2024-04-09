2383fafb0d0a76d32525292045eef7bf647f1fc4
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-10 11:32:37 +1200

    made print as list

--- connect.py
+++ connect.py
@@ -3,6 +3,40 @@ import sqlite3
 
 is_id = ""
 
+mains = []
+subs = []
+specials = []
+
+
+def get_all_mains():
+    db = sqlite3.connect("splatoon3.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT MainWeaponName FROM MainWeapon")
+    results = cursor.fetchall()
+    for i in results:
+        mains.append(i[0])
+    db.close()
+
+
+def get_all_subs():
+    db = sqlite3.connect("splatoon3.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT SubWeaponName FROM SubWeapon")
+    results = cursor.fetchall()
+    for i in results:
+        subs.append(i[0])
+    db.close()
+
+
+def get_all_specials():
+    db = sqlite3.connect("splatoon3.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT SpecialWeaponName FROM SpecialWeapon")
+    results = cursor.fetchall()
+    for i in results:
+        specials.append(i[0])
+    db.close()
+
 
 def check_id(results: list, id: str):
     target_id = id.replace("ID", "Name")
@@ -34,7 +68,8 @@ def connect(query):
         db.close()
         return results
     except Exception as e:
-        print(e, "fail ore")
+        print(e, "failure")
+        query()
 
 
 def print_result(results):
@@ -46,13 +81,44 @@ def print_result(results):
             print(i)
 
 
+def print_weapons_as_list():
+    db = sqlite3.connect("splatoon3.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM Weapons")
+    results = cursor.fetchall()
+    for i in range(len(results)):
+        if results[i][2]:
+            print(results[i][0], ", ", results[i][1], ", ", mains[int(results[i][2]) - 1], ", ", subs[int(results[i][3]) - 1], ", ", specials[int(results[i][4]) - 1])
 
-target = input("What are you looking for ")
-table = input("Where is it from ")
-limit = input("Do you want any filters leave blank if not ")
-filter = ""
-if target.lower().find("id") != -1:
-    is_id = target
-if limit:
-    filter = (f" WHERE {limit}")
-connect(f"SELECT {target} FROM {table}{filter}")
+
+def intro():
+    db = sqlite3.connect("splatoon3.db")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM Weapons")
+    column_names = [description[0] for description in cursor.description]
+    print("Column names:", column_names)
+    db.close()
+
+
+def query():
+    target = input("What are you looking for ")
+    table = input("Where is it from ")
+    limit = input("Do you want any filters leave blank if not ")
+    filter = ""
+    if target.lower().find("id") != -1:
+        is_id = target
+    if limit:
+        filter = (f" WHERE {limit}")
+    connect(f"SELECT {target} FROM {table}{filter}")
+
+
+def _ready():
+    get_all_mains()
+    get_all_subs()
+    get_all_specials()
+    intro()
+    print_weapons_as_list()
+    # query()
+
+
+_ready()
